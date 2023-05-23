cb15c57e01e31ec26b2666479f391dc6fabb8aa2
maeda-kento <k_maeda@concier.com>
2023-05-23 18:00:32 +0900

    スタッフ情報変更

--- app/controllers/staffs/registrations_controller.rb
+++ app/controllers/staffs/registrations_controller.rb
@@ -2,7 +2,7 @@ class Staffs::RegistrationsController < ApplicationController
   before_action :signed_in_staff, only: [:edit, :update, :destroy]
 
   def show
-    current_staff
+    @department = Department.find_by(id: current_staff.department_id)
   end
 
   def edit
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -5,7 +5,7 @@ class Staff < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  enum gender: { male: 1, female: 2, other: 0}
+  enum gender: { male: 1, female: 2, other: 0 }
   
 
   validates :last_name, presence: true, length: { maximum: 10 }
