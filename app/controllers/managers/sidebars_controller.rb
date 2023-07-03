e099d1afe228612246c24acbe2779d3b5905fa72
maeda-kento <k_maeda@concier.com>
2023-07-03 15:15:25 +0900

    サイドバー完成

--- app/controllers/managers/sidebars_controller.rb
+++ app/controllers/managers/sidebars_controller.rb
@@ -1,7 +1,13 @@
 class Managers::SidebarsController < ApplicationController
 
   def update
-    
+    if session[:sidebar] == "open"
+      params[:sidebar_status] = "close"
+      session[:sidebar] = params[:sidebar_status]
+    else
+      params[:sidebar_status] = "open"
+      session[:sidebar] = params[:sidebar_status]
+    end
   end
 
 end
